bc3a9cedd1cb73abc6a429dc839bdaf89ec1b0b7
Acute-Tri-angle <pptran106@student.hbuhsd.edu>
2024-09-20 10:58:51 -0700

    hehhehehheeh

--- Clock.java
+++ Clock.java
@@ -2,9 +2,31 @@ public class Clock
 {
   public static void main(String[] args)
   {
-    // test code here
+    timeOfDay(24);
   }
 
   // write code for timeOfDay here
-
+public static void timeOfDay (int x){
+  if (x == 12){
+    System.out.println("noon");
+  }
+  else if (x == 0){
+    System.out.println("midnight");
+  }
+  else if (x == 18){
+    System.out.println("dusk");
+  }
+  else if (x > 0 && x < 12){
+    System.out.println("morning");
+  }
+  else if (x > 12 && x < 18){
+    System.out.println("afternoon");
+  }
+  else if (x > 18 && x < 24){
+    System.out.println("evening");
+  }
+  else {
+    System.out.println("invalid input");
+  }
+}
 }
--- Coins.java
+++ Coins.java
@@ -2,9 +2,25 @@ public class Coins
 {
   public static void main(String[] args)
   {
-    // test code here
+    coinConverter(95268);
   }
 
   // write code for coinConverter
-
+public static void coinConverter (int x){
+  if (x<0){
+    System.out.println("invalid input");
+  }
+  else{
+    int y = x / 100;
+    System.out.println("Dollar bills: " + y);
+    y = x % 100 / 25;
+    System.out.println("Quarters: " + y);
+    y = x % 100 % 25 / 10;
+    System.out.println("Dimes: " + y);
+    y = x % 100 % 25 % 10 / 5;
+    System.out.println("Nickels: " + y);
+    y = x % 100 % 25 % 10 % 5;
+    System.out.println("Pennies: " + y);
+  }
+}
 }
